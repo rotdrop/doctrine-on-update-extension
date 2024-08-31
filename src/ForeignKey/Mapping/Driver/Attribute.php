06c2e5b0b9c7a8bcbde1abc2cf972943880d990e
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-08-31 18:07:27 +0200

    Add attribute driver

---
+++ src/ForeignKey/Mapping/Driver/Attribute.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Claus-Justus Heine
+ * @copyright 2021, 2024 Claus-Justus Heine <himself@claus-justus-heine.de>
+ */
+
+namespace CJH\Doctrine\Extensions\ForeignKey\Mapping\Driver;
+
+use CJH\Doctrine\Extensions\Mapping\Annotation\ForeignKey as AnnotationClass;
+
+use Gedmo\Mapping\Driver\AttributeDriverInterface;
+
+final class Attribute extends Annotation implements AttributeDriverInterface
+{
+
+}
